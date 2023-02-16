75a9c94fef608121b3a9ee94d3e54a119f8f2ba5
Benjamin Altpeter <hi@bn.al>
2023-02-16 12:31:16 +0100

    Basic support for traffic collection using mitmproxy
    Unfortunately, this currently returns the traffic as a string of the
    internal mitmproxy flows format, which isn't exactly helpful.
    
    I would prefer to return the traffic as a HAR but mitmproxy only
    supports that through a community addon and I'm not sure what the best
    way to load that here is.

--- src/util.ts
+++ src/util.ts
@@ -1,4 +1,23 @@
 import dns from 'dns';
+import type { ExecaChildProcess } from 'execa';
+import timeout from 'p-timeout';
 import { promisify } from 'util';
 
 export const dnsLookup = promisify(dns.lookup);
+
+export const killProcess = async (proc?: ExecaChildProcess) => {
+    if (proc) {
+        proc.kill();
+        await timeout(proc, { milliseconds: 15000 }).catch(() => proc.kill(9));
+    }
+};
+
+export const awaitProcessStart = (proc: ExecaChildProcess<string>, startMessage: string) =>
+    new Promise<true>((res) => {
+        proc.stdout?.addListener('data', (chunk: string) => {
+            if (chunk.includes(startMessage)) {
+                proc.stdout?.removeAllListeners('data');
+                res(true);
+            }
+        });
+    });
