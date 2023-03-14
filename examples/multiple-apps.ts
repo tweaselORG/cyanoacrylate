fc2de065a9d827321b63c0caddb5c96c2f5c270a
Lorenz Sieben <me@lorenz-sieben.com>
2023-03-14 12:28:49 +0100

    Add example for android emulator

---
+++ examples/multiple-apps.ts
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+import { readdirSync } from 'fs';
+import path from 'path';
+import { pause, startAnalysis } from '../src/index';
+
+// You can pass the following command line arguments:
+// `npx tsx examples/multiple-apps.ts <emulator name> <snapshot name> <paths to a folder of single apks>`
+
+(async () => {
+    const emulatorName = process.argv[2] || 'emulator-name';
+    const snapshotName = process.argv[3] || 'snapshot-with-setup-emu';
+    const apkFolder = process.argv[4] || 'path/to/app-files';
+
+    const analysis = startAnalysis({
+        platform: 'android',
+        runTarget: 'emulator',
+        capabilities: [],
+
+        targetOptions: {
+            snapshotName,
+            startEmulatorOptions: {
+                emulatorName,
+                proxy: '127.0.0.1:8080',
+            },
+        },
+    });
+
+    await analysis.ensureDevice();
+
+    // The library was designed to do this for many apps in one go,
+    // so you can easily loop through an array of apps.
+    for (const apkFile of readdirSync(apkFolder)) {
+        const appAnalysis = await analysis.startAppAnalysis({
+            main: path.join(apkFolder, apkFile),
+        });
+
+        await analysis.resetDevice();
+        await analysis.ensureTrackingDomainResolution();
+
+        await appAnalysis.installApp();
+        await appAnalysis.setAppPermissions();
+        await appAnalysis.startTrafficCollection();
+        await appAnalysis.startApp();
+
+        // Pause to wait for the app to generate network traffic.
+        await pause(6_000);
+
+        await appAnalysis.stopTrafficCollection();
+
+        const res = await appAnalysis.stop();
+        console.log(res);
+    }
+
+    await analysis.stop();
+})();
+/* eslint-enable no-console */
